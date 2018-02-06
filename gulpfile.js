d3ff932d58499e4efa9dc61335a9ab87243cadbe
Harsh Shah <harsh.shah@sapientrazorfish.com>
2018-02-06 12:47:45 +0530

    Automatic deployement setup

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,36 @@ var rename = require('gulp-rename');
 var svgmin = require('gulp-svgmin');
 var svgstore = require('gulp-svgstore');
 var stylefmt = require('stylefmt');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var importCss = require('postcss-import');
+var imageMin = require('gulp-imagemin');
+var htmlmin = require('gulp-htmlmin');
 
 gulp.task('styles', function() {
-  var plugins = [precss, autprefixer, cssnano];
-
-  return gulp
-    .src('./css/main.css')
+  var plugins = [importCss,precss, autprefixer, cssnano];
+  return gulp.src('./css/main.css')
     .pipe(postcss(plugins))
-    .pipe(gulp.dest('./prod/css/'));
+    .pipe(gulp.dest('./app/css/'));
+});
+
+gulp.task('imagemin',function(){
+  return gulp.src('./assets/**/*')
+    .pipe(imageMin())
+    .pipe(gulp.dest('./app/assets/'))
+});
+
+gulp.task('htmlmin',function(){
+  return gulp.src('./index.html')
+    .pipe(htmlmin())
+    .pipe(gulp.dest('./app/')) 
+})
+
+
+gulp.task('scripts',function(){
+    gulp.src(['./js/jquery-2.2.4.min.js' , './js/anime.min.js', './js/jquery.fittext.js', './js/typed.min.js', './js/jquery.fullpage.min.js', './js/TweenMax.min.js', './js/slick.min.js', './js/main.js'])
+      .pipe(concat('app.js'))
+      .pipe(gulp.dest('./app/js/'))
 });
 
 gulp.task('spritesvg', function() {
@@ -27,6 +49,21 @@ gulp.task('spritesvg', function() {
     .pipe(gulp.dest('./assets/'));
 });
 
-gulp.task('default', ['spritesvg', 'styles'], function() {
+
+gulp.task('build',[
+  'htmlmin',
+  'styles',
+  'imagemin',
+  'spritesvg',
+  'scripts'],function(){ })
+
+
+
+gulp.task('default',[
+  'htmlmin',
+  'styles',
+  'imagemin',
+  'spritesvg',
+  'scripts'], function() {
   gulp.watch('./css/**/*.css', ['styles']);
 });
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static('./'));
+app.use(express.static('./app/'));
 
 app.listen(PORT, err => {
   if (!err) {
