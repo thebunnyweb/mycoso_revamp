7f398e668d745bc7aeff6d3940d864bff64a77b1
Harsh Shah <harsh.shah@sapientrazorfish.com>
2018-02-06 14:56:53 +0530

    Changed Server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static('./app/'));
+
+app.use(express.static('app'));
 
 app.listen(PORT, err => {
   if (!err) {
