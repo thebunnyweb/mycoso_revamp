c963841414d50a8e33a0b73187b339d5edc197d3
Harsh Shah <harsh.shah@sapientrazorfish.com>
2018-02-07 14:47:22 +0530

    js framework changes

--- gulpfile.js
+++ gulpfile.js
@@ -15,29 +15,45 @@ var imageMin = require('gulp-imagemin');
 var htmlmin = require('gulp-htmlmin');
 
 gulp.task('styles', function() {
-  var plugins = [importCss,precss, autprefixer, cssnano];
-  return gulp.src('./css/main.css')
+  var plugins = [importCss, precss, autprefixer, cssnano];
+  return gulp
+    .src('./css/main.css')
     .pipe(postcss(plugins))
     .pipe(gulp.dest('./app/css/'));
 });
 
-gulp.task('imagemin',function(){
-  return gulp.src('./assets/**/*')
+gulp.task('imagemin', function() {
+  return gulp
+    .src('./assets/**/*')
     .pipe(imageMin())
-    .pipe(gulp.dest('./app/assets/'))
+    .pipe(gulp.dest('./app/assets/'));
 });
 
-gulp.task('htmlmin',function(){
-  return gulp.src('./*.html')
+gulp.task('htmlmin', function() {
+  return gulp
+    .src('./*.html')
     .pipe(htmlmin())
-    .pipe(gulp.dest('./app/')) 
-})
+    .pipe(gulp.dest('./app/'));
+});
 
+gulp.task('scripts', function() {
+  gulp
+    .src([
+      './js/jquery-2.2.4.min.js',
+      './js/anime.min.js',
+      './js/jquery.fittext.js',
+      './js/typed.min.js',
+      './js/jquery.fullpage.min.js',
+      './js/TweenMax.min.js',
+      './js/slick.min.js',
+      './js/jquery.validate.min.js'
+    ])
+    .pipe(concat('plugin.js'))
+    .pipe(gulp.dest('./app/js/'));
+});
 
-gulp.task('scripts',function(){
-    gulp.src(['./js/jquery-2.2.4.min.js' , './js/anime.min.js', './js/jquery.fittext.js', './js/typed.min.js', './js/jquery.fullpage.min.js', './js/TweenMax.min.js', './js/slick.min.js', './js/main.js', './js/form.js'])
-      .pipe(concat('app.js'))
-      .pipe(gulp.dest('./app/js/'))
+gulp.task('mainscripts', function() {
+  return gulp.src(['./js/main.js', './js/form.js']).pipe(gulp.dest('./app/js'));
 });
 
 gulp.task('spritesvg', function() {
@@ -49,21 +65,16 @@ gulp.task('spritesvg', function() {
     .pipe(gulp.dest('./assets/'));
 });
 
+gulp.task(
+  'build',
+  ['htmlmin', 'styles', 'imagemin', 'spritesvg', 'scripts','mainscripts'],
+  function() {}
+);
 
-gulp.task('build',[
-  'htmlmin',
-  'styles',
-  'imagemin',
-  'spritesvg',
-  'scripts'],function(){ })
-
-
-
-gulp.task('default',[
-  'htmlmin',
-  'styles',
-  'imagemin',
-  'spritesvg',
-  'scripts'], function() {
-  gulp.watch('./css/**/*.css', ['styles']);
-});
+gulp.task(
+  'default',
+  ['htmlmin', 'styles', 'imagemin', 'spritesvg', 'scripts','mainscripts'],
+  function() {
+    gulp.watch('./css/**/*.css', ['styles']);
+  }
+);
